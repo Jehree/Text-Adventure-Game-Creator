596a30381ab9d306fa8a96ae962cfc6041db7484
Jehree <126540981+Jehree@users.noreply.github.com>
2024-07-27 13:59:48 -0400

    initial commit

---
+++ JsonTools.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventureFramework
+{
+    public static class JsonTools
+    {
+        // Get the directory path of the current executable (.dll)
+        public static string assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? "";
+
+        public static void Write(string relativePath, JObject jsonObject)
+        {
+            // Combine the directory path with the relative path to get the full file path
+            string fullPath = Path.Combine(assemblyPath, relativePath);
+
+            // write JSON directly to a file
+            using (StreamWriter file = File.CreateText(fullPath))
+            using (JsonTextWriter writer = new JsonTextWriter(file)) {
+                jsonObject.WriteTo(writer);
+            }
+        }
+
+        public static JObject Read(string relativePath)
+        {
+            // Combine the directory path with the relative path to get the full file path
+            string fullPath = Path.Combine(assemblyPath, relativePath);
+
+            // read JSON directly from a file
+            using (StreamReader file = File.OpenText(fullPath))
+            using (JsonTextReader reader = new JsonTextReader(file)) {
+                return (JObject)JToken.ReadFrom(reader);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Runtime.CompilerServices;
+using Spectre.Console;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using TextAdventureFramework;
+
+Main main = new Main();
+main.Run();
+
+namespace TextAdventureFramework
+{
+    class Main
+    {
+        // Get the directory path of the current executable (.dll)
+        public string assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? "";
+
+        public void Run()
+        {
+            // Specify the relative path where you want to write the file
+            string relativePath = @"../somefolder/myjson.jsonc";
+
+            var dict = new Dictionary<string, object>
+            {
+                { "name", "John Doe" },
+                { "alive", true }
+            };
+
+            var json = JObject.FromObject(dict);
+
+            var json2 = JObject.FromObject(new Dictionary<string, object>
+            {
+                { "name", "John Doe" },
+                { "alive", true }
+            });
+
+            var json3 = new JObject(
+                new JProperty("name", "John Doe"),
+                new JProperty("alive", true));
+
+            JObject videogameRatings = new JObject(
+                new JProperty("Halo", 25),
+                new JProperty("Starcraft", 32),
+                new JProperty("Call of Duty", 7.5));
+
+
+            //JsonTools.Write(relativePath, json);
+            var readJson = JsonTools.Read(relativePath);
+            JsonTools.Write(relativePath, readJson);
+
+            AnsiConsole.Markup("[underline red]" + readJson.ToString() + "[/]");
+            Console.ReadLine();
+        }
+    }
+
+    class Example
+    {
+        // Get the directory path of the current executable (.dll)
+        public string assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? "";
+
+        public void Run()
+        {
+            // Specify the relative path where you want to write the file
+            string relativePath = @"../somefolder/myjson.jsonc";
+
+            var dict = new Dictionary<string, object>
+            {
+                { "name", "John Doe" },
+                { "alive", true }
+            };
+
+            var json = JObject.FromObject(dict);
+
+            var json2 = JObject.FromObject(new Dictionary<string, object>
+            {
+                { "name", "John Doe" },
+                { "alive", true }
+            });
+
+            var json3 = new JObject(
+                new JProperty("name", "John Doe"),
+                new JProperty("alive", true));
+
+            JObject videogameRatings = new JObject(
+                new JProperty("Halo", 25),
+                new JProperty("Starcraft", 32),
+                new JProperty("Call of Duty", 7.5));
+
+
+            //JsonTools.Write(relativePath, json);
+            var readJson = JsonTools.Read(relativePath);
+            JsonTools.Write(relativePath, readJson);
+
+            AnsiConsole.Markup("[underline red]" + readJson.ToString() + "[/]");
+            Console.ReadLine();
+        }
+}
+
+
+
+
+
+
